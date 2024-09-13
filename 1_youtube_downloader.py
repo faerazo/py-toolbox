ff71a9a309f2e976a37b6437b1f869944f0c9a99
faerazo <faerazo@espol.edu.ec>
2024-09-13 17:44:14 +0200

    concurrency implemented

--- 1_youtube_downloader.py
+++ 1_youtube_downloader.py
@@ -1,12 +1,12 @@
 import os
+from os.path import expanduser
 import logging
 import argparse
-#from pytube import YouTube
 from pytubefix import YouTube
 from moviepy.editor import AudioFileClip
 from pathlib import Path
-from os.path import expanduser
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
@@ -59,10 +59,22 @@ def process_video_download(url, save_path):
 
 def download_multiple_videos_as_mp3(urls, save_path):
     ensure_directory_exists(save_path)
-    downloaded_files = [
-        process_video_download(url.strip(), save_path) for url in urls if url.strip()
-    ]
-    downloaded_files = [file for file in downloaded_files if file]
+    downloaded_files = []
+    
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {executor.submit(process_video_download, url.strip(), save_path): url.strip() for url in urls if url.strip()}
+        
+        with tqdm(total=len(future_to_url), desc="Downloading and converting") as pbar:
+            for future in as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    file = future.result()
+                    if file:
+                        downloaded_files.append(file)
+                except Exception as exc:
+                    logging.error(f'{url} generated an exception: {exc}')
+                pbar.update(1)
+
     text_file = save_path / "downloaded_mp3_files.txt"
     with open(text_file, "w") as f:
         f.write("mp3_files = [\n")
